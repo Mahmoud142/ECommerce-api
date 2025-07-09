de1c379dad158a61438dacd5bccc2acc73b84e8c
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-07-09 14:59:42 +0300

    Login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -33,4 +33,34 @@ const registerUser = async (req, res) => {
     }
 }
 
-module.exports = { registerUser };
+// Login User 
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            return res.status(400).json({message:"Email and Password are required"});
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+        return res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error from user login' });
+    }
+}
+
+
+module.exports = { registerUser, loginUser };
