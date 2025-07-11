b83f387bb810bbb6b78c8c3be34ffe5e18ee3f08
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-07-11 19:24:13 +0300

    getAllProducts

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -24,6 +24,15 @@ const createProduct = async (req, res) => {
     }
 }
 
+const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({products : products });
+    }catch(error) {
+        res.status(500).json({ message: 'Server error while fetching products' });
+    }
+}
 module.exports = {
     createProduct
+    ,getAllProducts
 }
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { createProduct } = require('../controllers/product.controller');
+const { createProduct, getAllProducts } = require('../controllers/product.controller');
 const { protect,admin } = require('../middlewares/auth.middleware');
 
 // Route to create a new product
 router.post('/', protect, admin, createProduct);
-
+router.get('/', getAllProducts);
 module.exports = router;
