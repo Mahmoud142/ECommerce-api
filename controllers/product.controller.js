3bb186e309d837aebb08c787772214119ca57d9a
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-07-11 20:49:30 +0300

    delete product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -65,9 +65,22 @@ const updateProduct = async (req, res) => {
         res.status(500).json({message: 'Server error while updating product' });
     }
 }
+
+const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if(!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        res.status(500).json({message: 'Server error while deleting product' });
+    }
+}
 module.exports = {
     createProduct,
     getAllProducts,
     getProductById,
-    updateProduct
+    updateProduct,
+    deleteProduct
 }
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const { createProduct, getAllProducts, getProductById,updateProduct } = require('../controllers/product.controller');
-const { protect,admin } = require('../middlewares/auth.middleware');
+const {
+    createProduct,
+    getAllProducts,
+    getProductById,
+    updateProduct,
+    deleteProduct
+} = require('../controllers/product.controller');
+const { protect, admin } = require('../middlewares/auth.middleware');
 
 
 router.post('/', protect, admin, createProduct);
 router.get('/', getAllProducts);
 router.get('/:id', getProductById);
 router.put('/:id', protect, admin, updateProduct);
+router.delete('/:id', protect, admin, deleteProduct);
 module.exports = router;
