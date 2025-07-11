b63507bcd523d4cc146ae9c582a5216b9ff077ba
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-07-11 20:40:33 +0300

    delete product by ID

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -44,8 +44,30 @@ const getProductById = async (req, res) => {
         res.status(500).json({ message: 'Server error while fetching product by ID' });
     }
 }
+
+const updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        const {name , description, price, category, Image, brand, countInStock } = req.body;
+        product.name = name || product.name;
+        product.description = description || product.description;
+        product.price = price || product.price;
+        product.category = category || product.category;
+        product.Image = Image || product.Image;
+        product.brand = brand || product.brand;
+        product.countInStock = countInStock || product.countInStock;
+        const updatedProduct = await product.save();
+        res.status(200).json({ message: 'Product updated successfully', product: updatedProduct });
+    } catch (error) {
+        res.status(500).json({message: 'Server error while updating product' });
+    }
+}
 module.exports = {
     createProduct,
     getAllProducts,
-    getProductById
+    getProductById,
+    updateProduct
 }
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { createProduct, getAllProducts, getProductById } = require('../controllers/product.controller');
+const { createProduct, getAllProducts, getProductById,updateProduct } = require('../controllers/product.controller');
 const { protect,admin } = require('../middlewares/auth.middleware');
 
 
 router.post('/', protect, admin, createProduct);
 router.get('/', getAllProducts);
 router.get('/:id', getProductById);
-
+router.put('/:id', protect, admin, updateProduct);
 module.exports = router;
