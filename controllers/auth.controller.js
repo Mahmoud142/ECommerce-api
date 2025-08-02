719d76ffb293be21be4d5a710dca4e6c56ac34bd
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-08-02 21:01:09 +0300

    Modify and improve Login & Signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,66 +5,30 @@ const { SUCCESS, FAIL } = require('../utils/httpStatusText');
 const AppError = require('../utils/appError');
 const asyncWrapper = require('../middlewares/asyncWrapper');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
-const refreshAccessToken = asyncWrapper(async (req, res, next) => {
-    const cookies = req.cookies;
-    if (!cookies?.jwt) {
-        const err = AppError.create('No refresh token provided', 401, FAIL);
-        return next(err);
-    }
-    const refreshToken = cookies.jwt;
-
-    const user = await User.findOne({ refreshToken: refreshToken });
-    if (!user) {
-        const err = AppError.create('Forbidden: Invalid refresh token', 403, FAIL);
-        return next(err);
-    }
-    jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, decoded) => {
-        if (err || user._id.toString() !== decoded.id) {
-            const error = AppError.create('Invalid refresh token', 403, FAIL);
-            return next(error);
-        }
-        const accessToken = generateToken(decoded.id);
-        return res.status(200).json({ status: SUCCESS, token: accessToken });
-    });
-
-});
-
-const registerUser = asyncWrapper(async (req, res, next) => {
-
-    const { name, email, password, phone } = req.body;
-
-    const userExists = await User.findOne({ email });
-
-    if (userExists) {
-        const err = AppError.create('User already exists', 400, FAIL);
-        return next(err);
-    }
+//@desc signup a new user
+//@route POST /api/auth/register
+//@access Public
+exports.signup = asyncWrapper(async (req, res, next) => {
 
     const user = await User.create({
-        name,
-        email,
-        password,
-        phone
-    });
+        name: req.body.name,
+        slug: req.body.slug,
+        email: req.body.email,
+        password: req.body.password,
+        phone: req.body.phone,
+        profileImg: req.body.profileImg
+    })
 
     const token = generateToken(user._id);
-
-    return res.status(201).json({
-        status: SUCCESS,
-        message: 'User registered successfully',
-        data: {
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            phone: user.phone,
-            token: token,
-        }
-    });
+    res.status(201).json({ data: user, token });
 });
 
-
-const loginUser = asyncWrapper(async (req, res, next) => {
+//@desc login a user
+//@route POST /api/auth/login
+//@access Public
+exports.login = asyncWrapper(async (req, res, next) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
@@ -78,49 +42,13 @@ const loginUser = asyncWrapper(async (req, res, next) => {
     }
 
     const token = generateToken(user);
-    const refreshToken = generateRefreshToken(user);
-
-    user.refreshToken.push(refreshToken);
     await user.save();
-    
-    
-    res.cookie('jwt', refreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-    })
-    delete user._doc.password;
-    delete user._doc.refreshToken;
 
+
+    delete user._doc.password;
     return res.status(200).json({
         status: SUCCESS,
         message: 'User logged in successfully',
         data: { user, token }
     });
 });
-
-const logoutUser = asyncWrapper(async (req, res, next) => {
-
-    const cookies = req.cookies;
-    if (!cookies?.jwt) {
-        const err = AppError.create('No refresh token provided', 401, FAIL);
-        return next(err);
-    }
-    const refreshToken = cookies.jwt;
-    const user = await User.findOne({ refreshToken: refreshToken });
-    if (user) {
-        user.refreshToken = user.refreshToken.filter(token => token !== refreshToken);
-        await user.save();
-    }
-
-    res.clearCookie('jwt', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict'
-    });
-    res.status(200).json({ status: SUCCESS, message: 'Logged out successfully' });
-
-});
-
-module.exports = { registerUser, loginUser, refreshAccessToken, logoutUser };
