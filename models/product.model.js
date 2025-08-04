521a39b021513acb4e637e0a50e050774b50c5e4
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-08-04 20:38:16 +0300

    Remove review model from product model

--- models/product.model.js
+++ models/product.model.js
@@ -1,24 +1,5 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-    user: {
-        type:mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    rating: {
-        type: Number,
-        required: true
-    },
-    comment: {
-        type: String,
-        required: true
-    }
-})
 
 const productSchema = new mongoose.Schema(
     {
@@ -92,7 +73,6 @@ const productSchema = new mongoose.Schema(
             type: Number,
             default: 0
         },
-        reviews:[reviewSchema],
     }, {
     timestamps: true
     }
