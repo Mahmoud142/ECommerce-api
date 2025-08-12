fb86bee7804a1bc95481d760ed30caf4b18b8636
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-08-12 20:42:23 +0300

    Create Cash Order

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,144 +1,50 @@
-const Order = require('../models/order.model');
 const Product = require('../models/product.model');
-const { SUCCESS, FAIL } = require('../utils/httpStatusText');
+const Cart = require('../models/cart.model');
+const Order = require('../models/order.model');
+
 const AppError = require('../utils/appError');
+const { SUCCESS, FAIL } = require('../utils/httpStatusText');
 const asyncWrapper = require('../middlewares/asyncWrapper');
 
+//@desc Create a cash order
+//@route POST /api/orders/cash
+//@access Protected/User
+exports.createCashOrder = asyncWrapper(async (req, res, next) => {
 
-const createOrder = asyncWrapper(async (req, res, next) => {
+    const taxPrice = 0;
+    const ShippingPrice = 0;
 
-    const {
-        orderItems,
-        shippingAddress,
-        paymentMethod,
-        itemPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice } = req.body;
-
-    if (!shippingAddress || !paymentMethod || !orderItems || orderItems.length === 0) {
-        const err = AppError.create('All Fields are required', 400, FAIL);
-        return next(err);
+    // get cart
+    const cart = await Cart.findOne({ _id: req.params.cartId });
+    if (!cart) {
+        return next(AppError.create('Cart not found', 404, FAIL));
     }
+    const cartPrice = cart.totalAfterDiscount ? cart.totalAfterDiscount : cart.totalPrice;
 
-    if (itemPrice < 0 || taxPrice < 0 || shippingPrice < 0 || totalPrice < 0) {
-        const err = AppError.create('Prices cannot be negative', 400, FAIL);
-        return next(err);
-    }
-    for (let item of orderItems) {
-        const product = await Product.findById(item.product);
-        if (!product) {
-            const err = AppError.create(`Product with ID ${item.product} not found`, 400, FAIL);
-            return next(err);
-        }
-        if (item.quantity > product.countInStock) {
-            const err = AppError.create(`Not enough stock for product ${product.name}`, 400, FAIL);
-            return next(err);
-        }
-    }
-    const order = new Order({
+    // create order with cash
+    console.log("I am inside createCashOrder controller");
+    const order = await Order.create({
         user: req.user._id,
-        items: orderItems,
-        shippingAddress,
-        paymentMethod,
-        itemPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice
-    });
-    const createdOrder = await order.save();
-    for (let item of orderItems) {
-        const product = await Product.findById(item.product);
-        if (product) {
-            product.countInStock -= item.quantity;
-            await product.save();
-        }
-    }
-    res.status(201).json({ status: SUCCESS, message: "Order created successfully", data: { order: createdOrder } });
-});
+        cartItems: cart.cartItems,
+        shippingAddress: req.body.shippingAddress,
+        totalOrderPrice: req.body.totalOrderPrice
+    })
+    // update product quantity
+    if (order) {
+        const bulkOption = Array.isArray(cart.cartItems) ? cart.cartItems.map((item) => ({
+            updateOne: {
+                filter: { _id: item.product },
+                update: { $inc: { quantity: -item.quantity, sold: +item.quantity } }
+            },
+        })) : [];
+        await Product.bulkWrite(bulkOption, {});
 
-const getMyOrders = asyncWrapper(async (req, res, next) => {
-    const orders = await Order.find({ user: req.user._id }).sort({ createdAt: -1 });
-    res.status(200).json({ status: SUCCESS, message: "Orders fetched successfully", data: { orders: orders } });
-});
-
-const getOrderById = asyncWrapper(async (req, res, next) => {
-    const order = await Order.findById(req.params.id).populate('user', 'name email');
-    if (!order) {
-        const err = AppError.create('Order not found', 404, FAIL);
-        return next(err);
-    }
-    if (order.user._id.toString() === req.user._id.toString() || req.user.isAdmin) {
-        return res.status(200).json({ status: SUCCESS, message: "Order fetched successfully", data: { order: order } });
-    } else {
-        const err = AppError.create("You do not have permission to view this order", 403, FAIL);
-        return next(err);
+        await Cart.findByIdAndDelete(cart._id);
     }
 
-});
-
-const makeOrderAsPaid = asyncWrapper(async (req, res, next) => {
-
-    const order = await Order.findById(req.params.id);
-    if (!order) {
-        const err = AppError.create('Order not found', 404, FAIL);
-        return next(err);
-    }
-
-    if (req.user._id.toString() !== order.user.toString() && !req.user.isAdmin) {
-        const err = AppError.create("You do not have permission to update this order", 403, FAIL);
-        return next(err);
-    }
-    if (order.isPaid) {
-        const err = AppError.create("Order is already paid", 400, FAIL);
-        return next(err);
-    }
-
-    order.isPaid = true;
-    order.paidAt = Date.now();
-    order.paymentResult = { // I will modify it later based on Payment gateway
-        id: req.user.id || 'manual',
-        status: req.user.status || 'completed',
-        update_time: new Date().toISOString(),
-        email_address: req.user.email,
-    };
-
-    const updatedOrder = await order.save();
-
-    res.status(200).json({
+    res.status(201).json({
         status: SUCCESS,
-        message: "Order payment status updated",
-        data: { order: updatedOrder }
-    });
-});
-
-const markOrderAsDelivered = asyncWrapper(async (req, res, next) => {
-    const order = await Order.findById(req.params.id);
-    if (!order) {
-        const err = AppError.create('Order not found', 404, FAIL);
-        return next(err);
-    }
-    if (order.isDelivered) {
-        const err = AppError.create("Order is already delivered", 400, FAIL);
-        return next(err);
-    }
-
-    order.isDelivered = true;
-    order.deliveredAt = Date.now();
-    const updatedOrder = await order.save();
-    res.status(200).json({ status: SUCCESS, message: "Order marked as delivered", data: { order: updatedOrder } });
-});
-
-const getAllOrders = asyncWrapper(async (req, res, next) => {
-    const orders = await Order.find({}).populate('user', 'name email').sort({ createdAt: -1 });
-    res.status(200).json({ status: SUCCESS, message: "Orders fetched successfully", data: { orders: orders } });
-});
-
-module.exports = {
-    createOrder,
-    getMyOrders,
-    getOrderById,
-    makeOrderAsPaid,
-    markOrderAsDelivered,
-    getAllOrders,
-}
+        message: 'Order created successfully',
+        data: order
+    })
+})
