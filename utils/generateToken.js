2094694d14a7587132297d0ad42cb455abc3469e
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-08-12 21:48:49 +0300

    Secret The expiration Time

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
 const generateToken = (user) => {
-    return jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    return jwt.sign(
+        { id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 }
 
 const generateRefreshToken = (user) => {
-    return jwt.sign({ id: user._id }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
+    return jwt.sign({ id: user._id }, process.env.JWT_REFRESH_SECRET, { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN });
 }
 
 module.exports = { generateToken, generateRefreshToken };
