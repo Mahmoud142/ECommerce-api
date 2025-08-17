50708382e0428e266e2afe02a3717c662b8ed191
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-08-17 21:48:28 +0300

    Apply pagination to Subcategory

--- controllers/subcategory.controller.js
+++ controllers/subcategory.controller.js
@@ -18,10 +18,15 @@ exports.createSubCategory = asyncWrapper(async (req, res, next) => {
 // @route GET /api/subcategories
 //@access Public
 exports.getAllSubCategories = asyncWrapper(async (req, res, next) => {
-    const subCategories = await SubCategory.find();
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const skip = (page - 1) * limit;
+    const subCategories = await SubCategory.find().skip(skip).limit(limit);
     return res.status(200).json({
         status: SUCCESS,
-        data: { subCategories }
+        page,
+        length: subCategories.length,
+        data: { subCategories },
     });
 })
 
