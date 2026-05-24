274cc6c8dc2509db69719669a05d5d593d9b5380
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-05-25 01:30:29 +0300

    refactor: remove dynamic image URL formatting hook from Category model

--- models/category.model.js
+++ models/category.model.js
@@ -21,25 +21,7 @@ const categorySchema = new mongoose.Schema({
     image: String,
 
 }, { timestamps: true });
-
-const setImageUrl = (doc) => {
-    if (doc.image) {
-        const filename = doc.image.split('/').pop();
-        if (process.env.CLOUDINARY_CLOUD_NAME) {
-            doc.image = `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/categories/${filename}`;
-        } else {
-            doc.image = `${process.env.BASE_URL}/uploads/categories/${filename}`;
-        }
-    }
-};
-
-categorySchema.post('save', (doc) => {
-    setImageUrl(doc);
-});
-
-categorySchema.post('init', (doc) => {
-    setImageUrl(doc);
-});
+// category image hooks removed
 
 const Category = mongoose.model('Category', categorySchema);
 
