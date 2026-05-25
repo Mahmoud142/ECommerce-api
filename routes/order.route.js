fba667e1fa034342b9977818e2db5403edb5ea7d
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-05-25 15:32:16 +0300

    feat(routes): restrict order fulfillment actions to admin/manager and support checkout

--- routes/order.route.js
+++ routes/order.route.js
@@ -5,20 +5,23 @@ const {
     getAllOrders,
     getSingleOrder,
     updateOrderToPaid,
-    updateOrderToDelivered
+    updateOrderToDelivered,
+    checkoutSession
 } = require('../controllers/order.controller');
 
 const protect = require('../middlewares/protect.middleware');
+
+router.post('/checkout-session/:cartId', protect.auth, protect.allowedTo('user'), checkoutSession);
 
 router.route('/:cartId')
     .post(protect.auth, protect.allowedTo('user'), createCashOrder)
 
 router.route('/')
-    .get(protect.auth, protect.allowedTo('user'), getAllOrders)
+    .get(protect.auth, protect.allowedTo('user', 'admin'), getAllOrders)
 
 router.route('/:id')
-    .get(protect.auth, protect.allowedTo('user'), getSingleOrder)
+    .get(protect.auth, protect.allowedTo('user', 'admin'), getSingleOrder)
 
-router.put('/:id/pay', protect.auth, updateOrderToPaid) // add admin protect later
-router.put('/:id/deliver', protect.auth, updateOrderToDelivered) // add admin protect later
+router.put('/:id/pay', protect.auth, protect.allowedTo('admin', 'manager'), updateOrderToPaid)
+router.put('/:id/deliver', protect.auth, protect.allowedTo('admin', 'manager'), updateOrderToDelivered)
 module.exports = router;
